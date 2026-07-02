178644100bf097f29792856a947488b7d83b1586
will <670886335@qq.com>
2026-07-02 10:18:49 +0800

    chore: cleanup

--- src/link.ts
+++
@@ -1,2 +0,0 @@
-export { MarkdownLinksProvider } from "./features/links-tree/MarkdownLinksProvider";
-export { MarkdownLinkIndex as LinkIndexManager } from "./services/markdown-index/MarkdownLinkIndex";
